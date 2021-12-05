01847b70a2280b60beb898b32e685151c68c60f6
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-12-06 00:53:09 +0200

    Code cleanup

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -11,13 +11,17 @@ if (envFound.error) {
 export default {
   ENV: process.env.NODE_ENV as string,
   PORT: parseInt(process.env.PORT as string, 10) || 5000,
-  MONGODB_URI: process.env.MONGODB_URI as string,
-  LOGGING_DB_URI: process.env.LOGGING_DB_URI as string,
   SALT_FACTOR: parseInt(process.env.SALT_FACTOR as string) || 10,
   SEQUELIZE_CONFIG: sequelizeConfig[process.env.NODE_ENV as string],
-  MAIL_PORT: 587,
-  MAIL_HOST: 'smtp.ethereal.email',
-  MAILER_USER: process.env.MAILER_USER as string,
-  MAILER_PASS: process.env.MAILER_PASS as string,
-  MAIL_FROM: 'CreditGo',
+  MAILER: {
+    PORT: 587,
+    HOST: 'smtp.ethereal.email',
+    USER: process.env.MAILER_USER as string,
+    PASS: process.env.MAILER_PASS as string,
+    FROM: 'CreditGo',
+  },
+  REDIS: {
+    HOST: 'localhost',
+    PORT: 8089,
+  },
 };
