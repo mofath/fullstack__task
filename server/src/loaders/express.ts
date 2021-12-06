6d15f0804e4940735c4f42eebf37018985cd0b5e
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-12-07 01:56:22 +0200

    Add baseErrorhandler middleware

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,12 +1,16 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import api from '../api/routes';
+import { baseErrorHandlerMiddleware } from '../api/middleware';
 
 const expressLoader = async (app: Application) => {
   app.use(cors());
   app.use(express.json({ limit: '50mb', strict: false }));
   app.use(express.urlencoded({ extended: true }));
+
   app.use('/api/v1', api);
+
+  app.use(baseErrorHandlerMiddleware);
 };
 
 export default expressLoader;
