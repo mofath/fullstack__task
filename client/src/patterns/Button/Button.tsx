819926950ff85756c6e53150a2bb6ac524c937b7
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-12-08 00:42:47 +0200

    Add button component

---
+++ client/src/patterns/Button/Button.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string;
+  size: string;
+  onClick: () => void;
+  fullWidth: boolean;
+  className: string;
+}
+
+const Button: FC<Props> = ({
+  children,
+  size,
+  type,
+  onClick,
+  fullWidth,
+  className,
+}) => {
+  return (
+    <button
+      className={[
+        'button',
+        `button--${size}`,
+        `${className && className}`,
+        `${fullWidth ? 'button--fullWidth' : ''}`,
+      ].join('')}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
