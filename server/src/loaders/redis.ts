b1d51b2424adf0847dd0a1732a599d03b976f7c6
Mhmd Fathi <mohamed.elgaedi@gmail.com>
2021-12-09 06:04:35 +0200

    wip

--- server/src/loaders/redis.ts
+++ server/src/loaders/redis.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const redisLoader = async (logger: any) => {
+const redisLoader = async (logger) => {
   try {
     const client = createClient();
 
@@ -14,8 +14,42 @@ const redisLoader = async (logger: any) => {
 
     await client.connect();
 
-    return client;
-  } catch (error: any) {
+    const read = (key) => {
+      return new Promise((resolve, reject) => {
+        client.get(key, (err, reply) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          if (reply === null) {
+            resolve(null);
+            return;
+          }
+          resolve(JSON.parse(reply));
+        });
+      });
+    };
+
+    const write = (key, value, ttl) => {
+      return new Promise((resolve, reject) => {
+        let handler = (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve('');
+        };
+
+        client.set(key, JSON.stringify(value), handler);
+        if (ttl) client.expiry(key, ttl);
+      });
+    };
+
+    return {
+      read,
+      write,
+    };
+  } catch (error) {
     logger.error('Failed to load redis client');
     throw new Error(error);
   }
